ceef6414780e4dd33355c2a20f3eab3de600b088
themks666 <themks132@gmail.com>
2026-06-08 07:11:25 +0545

    day16 classwork

--- month1/week3/day16.py
+++ month1/week3/day16.py
@@ -30,4 +30,50 @@ std = Student()
 
 std.display()
 
-Polymorphism
+"""
+Polymorphism :
+    poly = many
+    morphism = forms
+    It is a feature of OOP in which a 'Method' have different forms
+    
+    --> it can be achieved in two ways :
+        1) method overriding
+        2) Method overloading
+        
+Method Overriding :
+    A process in which the method of parent class is overridie by the child class to the same method name 
+"""
+
+class Animal:
+    def sound(self):
+        print("Animal Sound")
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+        
+cat = Cat()
+
+cat.sound()
+
+class Calculation:
+    def area(self):
+        print("Area")
+class reactangle(Calculation):
+    def __init__(self, lenght, breadth):
+        self.length = lenght;
+        self.breadth = breadth
+    def area(self):
+        print("Area of reactangle : ", self.length*self.breadth)
+class Circle(Calculation):
+    def __init__(self, radius):
+        self.radius = radius;
+    def area(self):
+        print(f"Area of circle : {3.14 *self.radius**2}")
+        
+circle = Circle(3)
+circle.area()
+ract = reactangle(2,4 )
+ract.area()
